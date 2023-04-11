84855610eb996b5f4e99ee35c9e44a16c0672dae
Harikrushn96 <harikrushnpatel0073@gmail.com>
2023-04-12 00:03:36 +0100

    p

---
+++ vars/dockerPush.groovy
@@ -0,0 +1,14 @@
+def call(String hubUser, String project, String imgTag){
+    
+    withCredentials([usernamePassword(
+    credentialsId: 'dockerhub_credentials', 
+    passwordVariable: 'passwd', 
+    usernameVariable: 'user_name'
+    
+    )]) {
+        sh "docker login -u '$user_name' -p '$passwd'"
+    }
+    docker push ${hubUser}/${project}:${imgTag}
+    docker push ${hubUser}/${project}:latest
+}
+
