eda0b29512022dec85451ae20e8f92cf38b012d4
Harikrushn96 <harikrushnpatel0073@gmail.com>
2023-04-11 19:12:53 +0100

    e

--- gitCheckout.groovy
+++ gitCheckout.groovy
@@ -1,7 +1,7 @@
 def call(Map gitparams){
     checkout([
         $class : 'GitSCM'
-        branches: [{name: gitparams.branch}],
-        userRemoteConfig: [{url: gitparams.url}]
+        branches: [[name: gitparams.branch]],
+        userRemoteConfig: [[url: gitparams.url]]
     ])
 }
